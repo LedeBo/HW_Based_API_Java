444efd7f697d834bba2542d3d25d8ea7b135dcd7
Elena Bogdanova <denissova@bk.ru>
2023-07-06 11:56:35 +0300

    added solution to the Task1 for the FirstTask and Main files

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+/*
+1. Вычислить n-ое треугольного число (сумма чисел от 1 до n),
+ n! (произведение чисел от 1 до n)
+ */
+
 public class FirstTask {
-    
+
+    int n;
+    int sum;
+    int fakt;
+
+    public FirstTask() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число n");
+        n = in.nextInt();
+        sum = 0;
+        fakt = 1;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+            fakt *= i;
+        in.close();
+        }
+
+    }
+
+
+    public void triangleOutput() {
+      
+        System.out.println("сумма чисел от 1 до" +  n + "равна"  +  sum);
+    }
+    public void faktorialOutput() {      
+        
+        System.out.println("факториал числа" + n + "равен" + fakt);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void main(String[] args) {
+           FirstTask firstTask = new FirstTask();
+           firstTask.triangleOutput();
+           firstTask.faktorialOutput();
+    }   
 }
