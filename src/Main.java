027f8e9f580f741b0afc8e63687deac642dc2fda
Elena Bogdanova <denissova@bk.ru>
2023-07-06 12:08:03 +0300

    added 2 variants of solutions to the Task2 for the SecondTask and Main files

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-           FirstTask firstTask = new FirstTask();
-           firstTask.triangleOutput();
-           firstTask.faktorialOutput();
+        //    FirstTask firstTask = new FirstTask();
+        //    firstTask.triangleOutput();
+        //    firstTask.faktorialOutput();
+
+           SecondTask secondTask = new SecondTask();
+        //    secondTask.simpleNumber();
+           secondTask.simpleNum();
     }   
 }
--- src/SecondTask.java
+++ src/SecondTask.java
@@ -1,3 +1,44 @@
+/*
+2.  Вывести все простые числа от 1 до 1000
+ */
+
 public class SecondTask {
+
     
-}
+    // public void simpleNumber() {
+    //     for (int i = 2; i <=1000; i++) {
+    //         boolean Yes = true;
+    //         for (int j = 2; j < i; j ++) {
+    //             if (i % j == 0) {
+    //                 Yes = false;
+    //                 break;
+    //             }
+    //         }
+
+    //         if(Yes) {
+    //             System.out.println(i);
+    //         }
+    //     }
+    
+            public boolean simpleNumber(int number) {
+              
+                for (int i = 2; i * i <= number; i++) {
+                        if (number % i == 0) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+             
+            public void simpleNum() {
+                for (int number = 2; number <= 1000; number++) {
+                    if (simpleNumber(number)) {
+                    System.out.println(number);
+                        }
+                    }
+            }
+
+    }
+    
+// }
+
